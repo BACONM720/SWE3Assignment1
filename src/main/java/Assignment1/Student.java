d3a9e9b3635cd13d53197c61706780b188f07b32
BACONM720 <r.maguire6@nuigalway.ie>
2021-10-20 14:06:50 +0100

    Third Commit

--- src/main/java/Assignment1/Student.java
+++ src/main/java/Assignment1/Student.java
@@ -14,13 +14,13 @@ import org.joda.time.*;
  */
 public class Student {
 
-    private String name;
-    private int age;
-    private String dob;
-    private int id = 0;
-    private String username;
-   private ArrayList<Module> modules;
-   private ArrayList<Course> courses;
+    public String name;
+    public int age;
+  public String dob;
+  public int id = 0;
+    public String username;
+   public ArrayList<Module> modules;
+ public ArrayList<Course> courses;
 
     Student(String name, int age, String dob) {
         this.name = name;
